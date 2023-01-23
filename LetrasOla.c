657bd8ddb4bbd74adb443a23a4395c189c2622c6
HenriqueForte <a32771@tomazpelayo.com>
2023-01-23 14:30:14 +0000

    Update LetrasOla.c

--- LetrasOla.c
+++ LetrasOla.c
@@ -1,3 +1,10 @@
 // Resolva neste ficheiro o seu exercício
 
-
+#include<stdio.h>
+int main() {
+    printf(" ####    #       ##\n");
+    printf("#    #   #     #    #\n");
+    printf("#    #   #     ######\n");
+    printf(" ####    ##### #    #\n");
+    
+}
